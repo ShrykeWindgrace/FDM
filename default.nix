b8a6d103bf9b73b94c1e355a3d23ff0ce9aced58
Timofey ZAKREVSKIY <timofey.zakrevskiy@3ds.com>
2020-12-16 18:27:35 +0100

    Fix "tcrm" font problem

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
     name = "FDM";
     buildInputs = [
         gnumake
-        (texlive.combine { inherit (texlive) scheme-basic latexmk; })
+        (texlive.combine { inherit (texlive) scheme-basic latexmk cm-super; })
     ];
     src = pkgs.lib.sources.sourceFilesBySuffices ./. [".tex" ".pdf" "Makefile" ".png"];
 }
